b23aa7dc1732d2db45aea00da299747d65180ee3
مهند عادل <mohand90098@gmail.com>
2022-10-07 12:54:37 +0400

    small fixes

--- cart/models.py
+++ cart/models.py
@@ -17,9 +17,6 @@ class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return self.cart_id
-
     class Meta:
         db_table = "Cart"
 
@@ -45,4 +42,4 @@ class CartProduct(models.Model):
         return self.product.price * self.quantity
 
     def __str__(self):
-        return self.product.name
+        return self.product
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Cart, CartProduct, Product
